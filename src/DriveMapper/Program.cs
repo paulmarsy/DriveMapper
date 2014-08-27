e2e1d5c26323de6179590acf5f4c53745fb38d0b
Paul Marston <paul@marston.me>
2014-08-27 19:47:02 +0100

    Adding additional logging and debug information when an exception is thrown

--- src/DriveMapper/Program.cs
+++ src/DriveMapper/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using System.Text;
+using Microsoft.Win32;
 using System;
 using System.Windows.Forms;
 
@@ -6,16 +7,15 @@ namespace DriveMapper
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main(string[] args)
+        static void Main()
         {
             foreach (var mappedDriveConfig in MappedDriveConfig.GetAllConfigItems())
             {
+                var log = new StringBuilder();
                 try
                 {
+                    log.AppendFormat("Preparing to map {0} to {1}:\\", mappedDriveConfig.Share, mappedDriveConfig.DriveLetter);
                     var networkDrive = new NetworkDrive
                     {
                         Persistent = mappedDriveConfig.Persistent,
@@ -29,12 +29,20 @@ namespace DriveMapper
                         networkDrive.MapDrive();
                     else
                         networkDrive.MapDrive(mappedDriveConfig.Username, mappedDriveConfig.Password);
+                    log.AppendLine("Call to WNetAddConnection2A succeeded, about to rename drive label");
 
                     SetNetworkDriveLabel(mappedDriveConfig.Share, mappedDriveConfig.Label);
+
+                    log.AppendFormat("Successfully mapped and renamed network drive {0}:\\", mappedDriveConfig.DriveLetter);
                 }
                 catch (Exception e)
                 {
-                    MessageBox.Show("Error: " + e.Message);
+                    MessageBox.Show(string.Format("Error: {0}\n{1}\nInternal log:\n{2}\nStack Trace:\n{3}",
+                        e.Message,
+                        e.InnerException != null ? e.InnerException.Message : "No Inner Exception",
+                        log,
+                        e.StackTrace
+                        ), string.Format("Mapping network drive {0}:\\ failed", mappedDriveConfig.DriveLetter), MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }   
             }
         }
